758857b835e0e4a20509337263c74fe965d508b6
Mostafa Sameh <lambda.mostafa@gmail.com>
2016-04-05 17:02:17 +0100

    read all csv files

--- sensorcnn/dataset/examples.py
+++ sensorcnn/dataset/examples.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
 
 
 def load_from_csv(filename):
     """Load data from csv file and return pandas data frame"""
     names = ['x', 'y', 'z', 'label', '', '', '']
-    examples = pd.read_csv(filename, header=None, names=names)
+    dtype = {'x': pd.np.float32, 'y': pd.np.float32, 'z': pd.np.float32}
+    examples = pd.read_csv(filename, header=None, names=names, dtype=dtype)
     return examples
 
 
@@ -36,25 +38,36 @@ def label_examples(examples, labels=None):
     examples : pandas data frame represents all examples
     labels : list of all labels
     """
-    exercises = examples[examples['label'].notnull()]
-    if not labels:
-        labels_counts = exercises['label'].value_counts()
-        labels = [l for l in labels_counts.keys()]
-
     n_classes = len(labels)
     n_features = 3
     X = np.zeros((0, n_features))
     Y = np.zeros((0, n_classes))
 
-    for i, l in enumerate(labels):
-        xs = get_features(exercises[exercises['label'] == l])
-        X = np.append(X, xs, axis=0)
-        ys = np.zeros((xs.shape[0], n_classes))
-        ys[:51,i] = 1
-        Y = np.append(Y, ys, axis=0)
-
     not_exercises = examples[examples['label'].isnull()]
     xs_not = get_features(not_exercises)
     X = np.append(X, xs_not, axis=0)
     Y = np.append(Y, np.zeros((xs_not.shape[0], n_classes)), axis=0)
+
+    exercises = examples[examples['label'].notnull()]
+    if exercises.empty:
+        return X, Y
+
+    if labels is None:
+        labels_counts = exercises['label'].value_counts()
+        labels = [l for l in labels_counts.keys()]
+
+    for i, l in enumerate(labels):
+        xs = get_features(exercises[exercises['label'] == l])
+        X = np.append(X, xs, axis=0)
+        ys = np.zeros((xs.shape[0], n_classes))
+        ys[:51, i] = 1
+        Y = np.append(Y, ys, axis=0)
     return X, Y
+
+
+def csv_file_iterator(root_directory):
+    """Returns a generator (iterator) of absolute file paths for CSV files in a given directory"""
+    for root_path, _, files in os.walk(root_directory, followlinks=True):
+        for f in files:
+            if f.endswith("csv"):
+                yield os.path.join(root_path, f)
